1b5931a09d22d5dcd8deb8518837c351d96dc1f1
Björn Petersen <pheasn@gmail.com>
2018-09-19 16:46:42 +0200

    Install all manifests in CI
    Closes #2

---
+++ bin/install-all.ps1
@@ -0,0 +1,13 @@
+$failed = 0
+foreach ($file in Get-ChildItem) {
+    if ($file.Extension -eq ".json") {
+        $name = $file.Name
+        scoop install $name 6> install_$name.log
+        if (Get-Content install_$name.log | findstr -i "ERROR") {
+            $failed = 1
+        } else {
+            Remove-Item install_$name.log
+        }
+    }
+}
+exit $failed
