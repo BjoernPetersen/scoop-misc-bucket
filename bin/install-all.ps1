fdb547b78e8eb55af82792290ee2ddbb22c3517a
Björn Petersen <pheasn@gmail.com>
2018-09-19 17:02:58 +0200

    Use basename for log naming

--- bin/install-all.ps1
+++ bin/install-all.ps1
@@ -2,11 +2,12 @@ $failed = 0
 foreach ($file in Get-ChildItem) {
     if ($file.Extension -eq ".json") {
         $name = $file.Name
-        (scoop install $name 6> install_$name.log)
-        if (Get-Content install_$name.log | findstr -i "ERROR") {
+        $baseName = $file.BaseName
+        (scoop install $name 6> install_$baseName.log)
+        if (Get-Content install_$baseName.log | findstr -i "ERROR") {
             $failed = 1
         } else {
-            Remove-Item install_$name.log
+            Remove-Item install_$baseName.log
         }
     }
 }
