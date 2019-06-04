fe355ee85e1231163d25fdd81685ef8a396755f1
Björn Petersen <pheasn@gmail.com>
2019-06-04 14:42:33 +0200

    Move manifests to bucket/ dir

--- bin/install-all.ps1
+++ bin/install-all.ps1
@@ -1,7 +1,7 @@
 $failed = 0
-foreach ($file in Get-ChildItem) {
+foreach ($file in (Get-ChildItem .\bucket)) {
     if ($file.Extension -eq ".json") {
-        $name = $file.Name
+        $name = $file.FullName
         $baseName = $file.BaseName
         (scoop install $name 6> install_$baseName.log)
         if (Get-Content install_$baseName.log | findstr -i "ERROR") {
